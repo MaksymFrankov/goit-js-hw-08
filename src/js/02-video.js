4578e5734996965c9e3c3542e9869b19a331284a
Maksim Frankov <2m.franko@gmail.com>
2022-11-18 19:05:33 +0200

    fixes 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,9 +6,9 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", JSON.stringify(data)), 1000));
-
-const playerData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-
-player.setCurrentTime(playerData.seconds);
-
+let cat = localStorage.getItem('videoplayer-current-time');
+if (cat) {
+    const playerData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    player.setCurrentTime(playerData.seconds);
+}
 
