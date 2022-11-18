2918f1feb9c77941b44ff0bdf3a0e3ba726c6b77
Maksim Frankov <2m.franko@gmail.com>
2022-11-18 18:51:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,12 +6,8 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const playerData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-let seconds = 233;
-player.setCurrentTime(playerData.seconds).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    console.error(error)
-});
+
+player.setCurrentTime(playerData.seconds);
 
 player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", JSON.stringify(data)), 1000));
 
