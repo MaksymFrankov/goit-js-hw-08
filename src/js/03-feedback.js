b808a12787e9e0ab06177d8bb45645bc5544abfd
Maksim Frankov <2m.franko@gmail.com>
2022-11-18 18:15:23 +0200

    3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
+const textInput = document.querySelector(".feedback-form");
 
+textInput.addEventListener("input", (event) => SaveData(event.currentTarget.elements));
+
+const FormFeedback = document.querySelector('.feedback-form');
+FormFeedback.addEventListener('submit', (event) => FormSubmit(event));
+
+
+const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (data)
+{
+    FillForm(data);
+}
+
+function SaveData(data)
+{
+    d = {
+        email:    data['email'].value,
+        message: data['message'].value,
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(d));
+}
+
+function FillForm(data)
+{
+    let form = document.querySelector('.feedback-form');
+    form.elements['email'].value = data.email;
+    form.elements['message'].value = data.message;
+
+}
+
+function FormSubmit(event)
+{
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+}
