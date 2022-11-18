be23addadaac5efc9401cbf32314a6bb93edd612
Maksim Frankov <2m.franko@gmail.com>
2022-11-18 16:59:00 +0200

    2nd task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,68 +1,29 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
-const galleryContainer = document.querySelector('.gallery');
-const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = addItemCard(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-
-function createGalleryMarkup(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image"
-                    src="${preview}"
-                        data-source="${original}"
-                            alt="${description}" />
-  </a>
-</div>       
-`;
-  }).join('')
+function addItemCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+    `;
+    })
+    .join("");
 }
 
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
 
-const instance = basicLightbox.create(`
-  <div class="content">
-        <img class="image_light"
-          src=""
-          alt="full-image"
-        />
-    </div>
-  `, { onClose: (instance) => { document.removeEventListener("keydown", onEscKeyPress); } });
-galleryContainer.addEventListener("click", onClickCard);
-function onClickCard(evt) {
-  evt.preventDefault();
-
-  if ( !evt.target.classList.contains("gallery__image")) {
-    return;
-  }
-
-  let selectedImg = evt.target.dataset.source;
-  const loadImage = instance.element().querySelector("img");
-  loadImage.src = selectedImg;
 
   
-
-  instance.show();
-  let light = document.querySelector(".basicLightbox")
-  light.addEventListener("click", CloseModal);
-  document.addEventListener("keydown", onEscKeyPress);
-
-}
-
-
-function onEscKeyPress(event) {
-  if (event.code === "Escape") {
-    CloseModal();
-  }
-}
-
-function CloseModal() {
-  instance.close();
-  // document.removeEventListener("keydown", onEscKeyPress);
-}
+ 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player"
+import throttle from "lodash.throttle";
+ 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const playerData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(playerData.seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    console.error(error)
+});
+
+player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", JSON.stringify(data)), 1000));
 
